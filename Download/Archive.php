28dd0b1282917314ad3402f17d2ae3be8c7bbccc
Michael Wallner <mike@php.net>
2005-03-30 18:12:14 +0000

    * new docblock header
    git-svn-id: http://svn.php.net/repository/pear/packages/HTTP_Download/trunk@183181 c90b9560-bf6c-de11-be94-00142212c4b1

--- Download/Archive.php
+++ Download/Archive.php
@@ -1,25 +1,24 @@
 <?php
-// +----------------------------------------------------------------------+
-// | PEAR :: HTTP :: Download :: Archive                                  |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is available at http://www.php.net/license/3_0.txt              |
-// | If you did not receive a copy of the PHP license and are unable      |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2003-2005 Michael Wallner <mike@iworks.at>             |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * Send Archives created on-the-fly.
+ * HTTP::Download::Archive
  * 
- * @author      Michael Wallner <mike@php.net>
- * @package     HTTP_Download
- * @category    HTTP
- * @license     PHP License
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   HTTP
+ * @package    HTTP_Download
+ * @author     Michael Wallner <mike@php.net>
+ * @copyright  2003-2005 Michael Wallner
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/HTTP_Download
  */
 
 /**
