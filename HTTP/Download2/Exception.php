a53964352ca9ce0ee55860b4205a482cc82703bc
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-01-15 01:29:20 +1030

    PHPCS

--- HTTP/Download2/Exception.php
+++ HTTP/Download2/Exception.php
@@ -1,2 +1,28 @@
 <?php
-class HTTP_Download2_Exception extends Exception {}
+/**
+ * HTTP_Download2
+ *
+ * PHP version 5
+ *
+ * @category  HTTP
+ * @package   HTTP_Download2
+ * @author    Michael Wallner <mike@php.net>
+ * @copyright 2003-2005 Michael Wallner
+ * @license   BSD, revised
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/HTTP_Download2
+ */
+
+/**
+ * An exception for HTTP_Download2
+ *
+ * @category  HTTP
+ * @package   HTTP_Download2
+ * @author    Daniel O'Connor <clockwerx@php.net>
+ * @copyright 2012 Daniel O'Connor
+ * @license   BSD, revised
+ * @link      http://pear.php.net/package/HTTP_Download2
+ */
+class HTTP_Download2_Exception extends Exception
+{
+}
